b196edc96a1d3d8b3b271f518f938f1420aa1658
Bellocci Francesco <francesco.bellocci@stud.unifi.it>
2021-06-29 16:29:24 +0200

    Modified class

---
+++ testCoveralls/src/main/java/com/examples/testCoveralls/model/Factorial.java
@@ -0,0 +1,12 @@
+package com.examples.testCoveralls.model;
+
+public class Factorial {
+
+	public int compute(int n) {
+		if (n < 0)
+			throw new IllegalArgumentException("Negative input: " + n);
+		if (n < 2)
+			return 1;
+		return n * compute(n - 1);
+	}
+}
---
+++ testCoveralls/src/test/java/cover/examples/testCoveralls/model/FactorialTest.java
@@ -0,0 +1,56 @@
+package cover.examples.testCoveralls.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.examples.testCoveralls.model.Factorial;
+
+public class FactorialTest {
+
+	@SuppressWarnings("deprecation")
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	private Factorial factorial;
+
+	@Before
+	public void setup() {
+		factorial = new Factorial();
+	}
+
+	@Test
+	public void testFactorialOf0() {
+		assertEquals(1, factorial.compute(0));
+	}
+
+	@Test
+	public void testFactorialOf1() {
+		assertEquals(1, factorial.compute(1));
+	}
+
+	@Test
+	public void testFactorialOf2() {
+		assertEquals(2, factorial.compute(2));
+	}
+
+	@Test
+	public void testFactorialOf3() {
+		assertEquals(6, factorial.compute(3));
+	}
+
+	@Test
+	public void testFactorialOf4() {
+		assertEquals(24, factorial.compute(4));
+	}
+
+	@Test
+	public void testNegativeInput() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Negative input: -1");
+		factorial.compute(-1);
+	}
+}
